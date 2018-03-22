8da9c72847b647e8f669bf86d5b4ff2070e3f785
Elliott Grieco <elliott.grieco@gmail.com>
2018-03-22 13:25:35 -0400

    Initial configuration for testing environment

---
+++ karma.conf.js
@@ -0,0 +1,19 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ['jasmine'],
+    reporters: ['spec'],
+    browsers: ['PhantomJS'],
+    files: [
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'node_modules/@babel/polyfill/dist/polyfill.js',
+      'js/**/*.js',
+      'spec/**/*.js',
+      'spec/**/*.spec.js'
+    ],
+    preprocessors: {
+      'js/**/*.js': ['babel'],
+      'spec/**/*.spec.js': ['babel']
+    }
+  });
+};
